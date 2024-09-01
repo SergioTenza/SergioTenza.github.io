ed9bf65e6c7b6965cdd4bcabc680f66aee46aab5
Sergio Tenza <sergeix77@gmail.com>
2024-09-01 07:53:26 +0200

    EMAIL_CODING

--- SergioTenza.Blazor.Wasm/SergioTenza.Blazor.Wasm/Services/MailService.cs
+++ SergioTenza.Blazor.Wasm/SergioTenza.Blazor.Wasm/Services/MailService.cs
@@ -1,20 +1,32 @@
 using System;
-using FluentEmail.Core;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace SergioTenza.Blazor.Wasm.Services;
 
 public class MailService
 {
-    public async Task<bool> SendMailNotification(string sender, string message,string name,string position)
+    public async Task<bool> SendMailNotification(string sender, string message, string name, string position)
     {
+        var messageMime = new MimeMessage();
+        messageMime.From.Add(new MailboxAddress(name, sender));
+        messageMime.To.Add(new MailboxAddress("TNZ Servicios Informaticos", "info@tnzservicios.es"));
+        messageMime.Subject = $"Web contact from {name} - {position}";
 
-        var email = await Email
-            .From(sender)
-            .To("info@tnzservicios.es", "contact info")
-            .Subject($"Web contact form {name}: {position}")
-            .Body(message)
-            .SendAsync();
-        return email.Successful;
+        messageMime.Body = new TextPart("plain")
+        {
+            Text = message
+        };
 
+        using (var client = new SmtpClient())
+        {
+            client.Connect("smtp.ionos.es", 465, true);
+
+            client.Authenticate("info@tnzservicios.es", "password");
+
+            client.Send(messageMime);
+            client.Disconnect(true);
+            return false;
+
+        }
     }
-}
